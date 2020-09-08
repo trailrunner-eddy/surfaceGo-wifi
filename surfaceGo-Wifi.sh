f64cfa436be0b816816a7a9bfdedb8c44b9ad2eb
Eddy Allen <allen-edward@hotmail.com>
2020-09-08 08:57:02 -0700

    Add files via upload
    Having a weird bug where the download doesn't seem to work the first time.

--- surfaceGo-Wifi.sh
+++ surfaceGo-Wifi.sh
@@ -6,7 +6,7 @@
 # Checking to make sure board.bin is present
 if [[ -f ~/Downloads/board.bin ]]
 then
-    echo "You have the file."
+    echo "You have the file. Requesting sudo access to rm and cp the file."
     # Replacing the old board.bin file
     sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
     sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
@@ -15,6 +15,23 @@ then
     sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
     sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
 else
-    echo "Please download the board.bin file"
+    # Providing the download within the script
+    echo "Would you like to download the file [y/n]?"
+    read input
+    if [[ $input == 'y' || $input == 'Y' ]]
+    then
+        wget http://www.killernetworking.com/support/K1535_Debian/board.bin -P ~/Downloads/
+        echo "File downloaded"
+        # Replacing the old board.bin file
+        sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
+        sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
+
+        # Same as above, just different location
+        sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
+        sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
+    else
+        echo "Make sure you place the board.bin file in ~/Downloads"
+        echo "Download link: http://www.killernetworking.com/support/K1535_Debian/board.bin "
+    fi    
 fi
 echo "Done!"
