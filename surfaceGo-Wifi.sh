24beb65ad1856c93dee5fb013d73c422575386e7
Eddy Allen <allen-edward@hotmail.com>
2020-09-08 08:38:12 -0700

    Add files via upload

--- surfaceGo-Wifi.sh
+++ surfaceGo-Wifi.sh
@@ -3,12 +3,18 @@
 # Simple shell script to autofix no wifi support on Kali Linux on a Surface Go
 # Requires board.bin file in ~/Downloads/
 
-# Replacing the old board.bin file
-sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
-sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
+# Checking to make sure board.bin is present
+if [[ -f ~/Downloads/board.bin ]]
+then
+    echo "You have the file."
+    # Replacing the old board.bin file
+    sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
+    sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
 
-# Same as above, just different location
-sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
-sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
-
-echo "Done"
+    # Same as above, just different location
+    sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
+    sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
+else
+    echo "Please download the board.bin file"
+fi
+echo "Done!"
