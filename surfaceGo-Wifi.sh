72c28c1946d745540fe3b2ab3ff058d5ac49ccea
Eddy Allen <allen-edward@hotmail.com>
2020-09-08 09:03:49 -0700

    Add files via upload

--- surfaceGo-Wifi.sh
+++ surfaceGo-Wifi.sh
@@ -15,23 +15,6 @@ then
     sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
     sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
 else
-    # Providing the download within the script
-    echo "Would you like to download the file [y/n]?"
-    read input
-    if [[ $input == 'y' || $input == 'Y' ]]
-    then
-        wget http://www.killernetworking.com/support/K1535_Debian/board.bin -P ~/Downloads/
-        echo "File downloaded"
-        # Replacing the old board.bin file
-        sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
-        sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
-
-        # Same as above, just different location
-        sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
-        sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
-    else
-        echo "Make sure you place the board.bin file in ~/Downloads"
-        echo "Download link: http://www.killernetworking.com/support/K1535_Debian/board.bin "
-    fi    
+    echo "Make sure you download the board.bin file and place it in ~/Downloads/"
 fi
-echo "Done!"
+echo "Done! Reboot for wifi to work!"
