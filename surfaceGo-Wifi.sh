a9dcdc2094bc7ea1159b0f9155953f5805d494c3
Eddy Allen <allen-edward@hotmail.com>
2020-09-09 08:53:14 -0700

    Add files via upload

--- surfaceGo-Wifi.sh
+++ surfaceGo-Wifi.sh
@@ -3,7 +3,7 @@
 # Simple shell script to autofix no wifi support on Kali Linux on a Surface Go
 # board.bin file needs to be in your ~/Downloads/ folder
 # If you do not have the board.bin file and are connected via ethernet the script can download the file
-# board.bin file was taken from: https://www.killernetworking.com/support/K1535_Debian
+# board.bin file is from: https://www.killernetworking.com/support/K1535_Debian/board.bin
 # I uploaded the file to GitHub incase the killernetworking link ever goes dead.
 
 # Checking to make sure board.bin is present
@@ -24,7 +24,7 @@ else
     read input
     if [[ $input == 'y' || $input == 'Y' ]]
     then
-        wget https://github.com/trailrunner-eddy/surfaceGo-wifi/blob/master/board.bin -P ~/Downloads/
+        wget https://www.killernetworking.com/support/K1535_Debian/board.bin -P ~/Downloads/
         echo "File downloaded"
         # Replacing the old board.bin file
         sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
@@ -35,7 +35,8 @@ else
         sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
     else
         echo "Make sure you place the board.bin file in ~/Downloads"
-        echo "Download link: https://github.com/trailrunner-eddy/surfaceGo-wifi/blob/master/board.bin "
+        echo "Download link: https://www.killernetworking.com/support/K1535_Debian/board.bin "
     fi    
 fi
 echo "Done!" 
+sudo shutdown -P now
