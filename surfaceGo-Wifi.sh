a3c5687e360ec4e8e5f661e4611089bc61221b30
Eddy Allen <allen-edward@hotmail.com>
2020-09-09 07:54:27 -0700

    Add files via upload
    Re-added the download feature since a reddit user told me that it worked for him while he was using an Ethernet cable.

--- surfaceGo-Wifi.sh
+++ surfaceGo-Wifi.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Simple shell script to autofix no wifi support on Kali Linux on a Surface Go
-# Requires board.bin file in ~/Downloads/
+# board.bin file needs to be in your ~/Downloads/ folder
+# If you do not have the board.bin file and are connected via ethernet the script can download the file
+# board.bin file was taken from: https://www.killernetworking.com/support/K1535_Debian
+# I uploaded the file to GitHub incase the killernetworking link ever goes dead.
 
 # Checking to make sure board.bin is present
 if [[ -f ~/Downloads/board.bin ]]
@@ -11,10 +14,28 @@ then
     sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
     sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
 
-    # Same as above, just different location
+    # Replacing the old board.bin file in second location
     sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
     sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
 else
-    echo "Make sure you download the board.bin file and place it in ~/Downloads/"
+    echo "Please download the board.bin file"
+    # Providing the download within the script
+    echo "Would you like to download the file [y/n]?"
+    read input
+    if [[ $input == 'y' || $input == 'Y' ]]
+    then
+        wget https://github.com/trailrunner-eddy/surfaceGo-wifi/blob/master/board.bin -P ~/Downloads/
+        echo "File downloaded"
+        # Replacing the old board.bin file
+        sudo rm /lib/firmware/ath10k/QCA6174/hw2.1/board.bin
+        sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw2.1
+
+        # Same as above, just different location
+        sudo rm /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
+        sudo cp ~/Downloads/board.bin /lib/firmware/ath10k/QCA6174/hw3.0
+    else
+        echo "Make sure you place the board.bin file in ~/Downloads"
+        echo "Download link: https://github.com/trailrunner-eddy/surfaceGo-wifi/blob/master/board.bin "
+    fi    
 fi
-echo "Done! Reboot for wifi to work!"
+echo "Done!" 
